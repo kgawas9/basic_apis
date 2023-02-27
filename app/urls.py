32b3fecb3997528aca77d687006fdcc860c880b8
Kiran <kgawas9@gmail.com>
2023-02-27 23:49:41 +0530

    basic put patch and delete request

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home , name='home'),
-    path('create/', views.post_student_data, name='created-record'),
+    path('create/', views.post_student_data, name='created-stud-record'),
+    path('update/<int:id>/', views.update_student_data, name='update-stud-record'),
+    path('delete/<int:id>/', views.delete_student_data, name='delete-stud-record'),
 ]
--- app/views.py
+++ app/views.py
@@ -41,3 +41,78 @@ def post_student_data(request):
         'payload': request.data,
         'message': 'New entry successfully created'
     })
+
+@api_view(['PUT'])
+def update_student_data(request, id):
+    try:
+        student_obj = Student.objects.get(pk=id)
+    
+        data = request.data
+
+        serializer = StudentSerializer(student_obj, data=data)
+
+        if not serializer.is_valid():
+            return Response({
+                'status': 403,
+                'error': serializer.errors,
+                'message': 'invalid input'
+            })
+        
+        serializer.save()
+        return Response({
+            'status': 200,
+            'payload': serializer.data,
+            'message': 'Data successfully updated - put request'
+        })
+    
+    except Exception as e:
+        return Response({
+            'status': 403,
+            'error': 'invalid id' + str(e)
+        })
+
+@api_view(['PATCH'])
+def update_student_data(request, id):
+    try:
+        student_obj = Student.objects.get(pk=id)
+    
+        data = request.data
+
+        serializer = StudentSerializer(student_obj, data=data, partial = True)
+
+        if not serializer.is_valid():
+            return Response({
+                'status': 403,
+                'error': serializer.errors,
+                'message': 'invalid input'
+            })
+        
+        serializer.save()
+        return Response({
+            'status': 200,
+            'payload': serializer.data,
+            'message': 'Data successfully updated - patch request'
+        })
+    
+    except Exception as e:
+        return Response({
+            'status': 403,
+            'error': 'invalid id' + str(e)
+        })
+
+
+@api_view(['DELETE'])
+def delete_student_data(request, id):
+    try:
+        stud_obj = Student.objects.get(id=id)
+        stud_obj.delete()
+
+        return Response({
+            'status': 200,
+            'message': 'record delete'
+        })
+    except Exception as e:
+        return Response({
+            'status': 403,
+            'message': 'Unable to delete record ' + str(e)
+        })
