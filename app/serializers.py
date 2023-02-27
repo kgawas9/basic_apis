befeb4a104a9610ff80647e5dc9efaec5a4a4a8e
Kiran <kgawas9@gmail.com>
2023-02-27 23:27:58 +0530

    added basic validations

--- app/serializers.py
+++ app/serializers.py
@@ -10,3 +10,22 @@ class StudentSerializer(serializers.ModelSerializer):
         ]
         # exlcude = ['']
         # fields = '__all__'
+
+    def validate(self, data):
+        if data['age'] < 18:
+            raise serializers.ValidationError({
+                'error':'age cannot be less than 18'
+                })
+        
+        if data['name']:
+            for i in data['name']:
+                if i.isdigit():
+                    raise serializers.ValidationError({
+                'error':'name should not contain any numbers'
+            })
+        else:
+            raise serializers.ValidationError({
+                'error':'name should not be blank'
+            })
+        
+        return data
